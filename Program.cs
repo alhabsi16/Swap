edbf061229c2b93de521f42ea5b52a2d29cfd4a3
alhabsi16 <mxmx8283@gmail.com>
2023-05-31 19:45:27 +0400

    update

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace swap1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //swap a and b
+            int a = 5;
+            int b = 9;
+            int c = 0;
+
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            //swap hello and world
+            string d = "hello";
+            string e = "world";
+            string f = "";
+
+            f = d;
+            d = e;
+            e = f;
+
+            Console.WriteLine("********************************");
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+        }
+    }
+}
+        
